0d58d5b425d31045bb172f29af5de04b45e3ee5a
kamal <donga.kimo14@gmail.com>
2023-10-08 08:45:49 +0300

    add new fils movie

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -5,7 +5,7 @@ export default function Navbar() {
     
     <nav className="navbar bg-light navbar-expand-lg bg-body-tertiary">
   <div className="container-fluid">
-    <Link className="navbar-brand" to="home">Navbar</Link>
+    <Link className="navbar-brand" to="">Navbar</Link>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
@@ -13,7 +13,7 @@ export default function Navbar() {
       <ul className="navbar-nav me-auto mb-2 mb-lg-0">
 
         <li className="nav-item">
-          <Link className="nav-link" to="home">Home</Link>
+          <Link className="nav-link" to="">Home</Link>
         </li>
         <li className="nav-item">
           <Link className="nav-link" to="about">About</Link>
